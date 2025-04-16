c0bcac241ec54f56a7878b89791f069b25486441
tung3110 <tungnh@outlook.com>
2025-04-16 22:33:41 +0700

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
+sudo apt -y update
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt -y install docker-ce
 sudo usermod -aG docker $USER
-sudo apt install docker-compose
-sudo apt update
+sudo apt -y install docker-compose
+sudo apt -y update
